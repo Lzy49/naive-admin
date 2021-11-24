fd3728bc487037fdba2491962a9b90e33eed5db9
lzy <362018513@qq.com>
2021-11-24 17:08:04 +0800

    因为图片依然有看不清的情况，所以设置为点击放大模式

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,19 +1,27 @@
 import { defineComponent } from 'vue';
-import { NAvatar, NPopover, NButton } from 'naive-ui';
+import { NAvatar, NPopover, NImage } from 'naive-ui';
 export default defineComponent({
   props: {
     src: String,
+    size: {
+      type: Number,
+      default: 50
+    }
   },
   render() {
     return (
-      <NPopover
-        {...this.$attrs}
-        v-slots={{
-          trigger: () => <NAvatar src={this.src} {...this.$attrs} />
+      <div
+        style={{
+          width: `${this.size}px`,
+          height: `${this.size}px`,
+          overflow: 'hidden',
+          borderRadius: '50%',
+          display: 'inline-flex',
+          
         }}
       >
-        <img src={this.src} width="200" />
-      </NPopover>
+        <NImage width="50" src={this.src} />
+      </div>
     );
   }
 });
